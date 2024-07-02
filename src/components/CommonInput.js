89802f431f7bc2952a2c01e39e183df1f3fc0b94
Vivek A <vivek.avlani@gmail.com>
2024-07-02 12:58:00 -0500

    Replaced FaPlay icon with FaShareSquare icon and added background color to buttons in CommonInput component.

--- src/components/CommonInput.js
+++ src/components/CommonInput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaPaperPlane, FaCopy, FaPlay } from 'react-icons/fa';
+import { FaPaperPlane, FaCopy, FaShareSquare } from 'react-icons/fa';
 
 const CommonInput = ({ input, setInput, handleSubmit, handleCopy, handleSendIndividual }) => {
   return (
@@ -10,13 +10,13 @@ const CommonInput = ({ input, setInput, handleSubmit, handleCopy, handleSendIndi
         onChange={(e) => setInput(e.target.value)}
         placeholder="Type a message to send to all chat windows..."
       />
-      <button type="button" className="copy-button" onClick={handleCopy} title="Copy to all inputs">
+      <button type="button" className="copy-button" onClick={handleCopy} title="Copy to all inputs" style={{backgroundColor: '#d2b48c'}}>
         <FaCopy />
       </button>
-      <button type="button" className="send-individual-button" onClick={handleSendIndividual} title="Send from individual inputs">
-        <FaPlay />
+      <button type="button" className="send-individual-button" onClick={handleSendIndividual} title="Send from individual inputs" style={{backgroundColor: '#d2b48c'}}>
+        <FaShareSquare />
       </button>
-      <button type="submit" className="send-button" title="Send to all chat windows">
+      <button type="submit" className="send-button" title="Send to all chat windows" style={{backgroundColor: '#d2b48c'}}>
         <FaPaperPlane />
       </button>
     </form>
